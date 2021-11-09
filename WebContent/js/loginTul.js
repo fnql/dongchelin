faf05d5de65c42f48f35c15b219cc0aa458772c9
fnql <dbwhddnr00@naver.com>
2021-11-09 10:34:34 +0900

    파일 정리

--- WebContent/js/loginTul.js
+++ WebContent/js/loginTul.js
@@ -1,3 +1,13 @@
-$("#logintul").click(function(){
-    $("#panel").slideDown();
+$(document).ready(function(){
+  $("#logintul").click(function(){
+    $("#panel").slideToggle("slow");
   });
+});
+
+$(document).ready(function() {
+  $('#summernote').summernote({
+      tabsize: 2,
+      height: 300,
+      lang: 'ko-KR' // 언어 세팅
+  });
+});
