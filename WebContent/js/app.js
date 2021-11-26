1c250707824ccace60391b95d05c02116090b114
fnql <dbwhddnr00@naver.com>
2021-11-26 10:30:55 +0900

    mypage 기초완성

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -14,3 +14,14 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+var maxIndex = $("li.graph-bar").length;
+
+for(var i=0; i<maxIndex; i++){
+  var val = $("li.graph-bar").eq(i).attr('graph-val');
+  $("li.graph-bar").eq(i).css({
+    "left": (i+1)*80+"px"
+  }).animate({
+    "height":val+"%"
+  },800);
+}
